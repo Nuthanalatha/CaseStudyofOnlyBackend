a31ad0b943bf9bd054b749ae6aa046b87911c0bf
Nuthana Gangadharappa <107018@tfcorp.com>
2022-12-14 10:20:51 +0530

    Casestudyofbackend

---
+++ CaseStudyForWordSearch/Data/Migrations/20221207140313_wordsearchsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CaseStudyForWordSearch.Data.Migrations
+{
+    public partial class wordsearchsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Word",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WordSearch = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Definition = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Word", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Word");
+        }
+    }
+}
---
+++ CaseStudyForWordSearch/Models/Word.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseStudyForWordSearch.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string WordSearch { get; set; }
+        public string Definition { get; set; }
+    }
+}
